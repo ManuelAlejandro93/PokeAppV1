b8eba9c4d8bc9ae8fb47ef1e0a32e2b4f4e7bcc4
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-25 18:20:44 -0500

    react bits instalado.

--- src/PokeApp.tsx
+++ src/PokeApp.tsx
@@ -1,3 +1,24 @@
+import { ClickSpark } from '@/reactbits';
+
 export function PokeApp() {
-  return <h1 className='text-red-300'>Soy un titulo, mi perro.</h1>;
+  return (
+    <ClickSpark
+      sparkColor='#EFBF04'
+      sparkSize={10}
+      sparkRadius={20}
+      sparkCount={8}
+      duration={400}
+    >
+      <div className='bg-black'>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+        <h1 className='text-red-800'>Soy un titulo, mi perro.</h1>
+      </div>
+    </ClickSpark>
+  );
 }
