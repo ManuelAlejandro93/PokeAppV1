ee8282486230adb557161aa77ce4186cb10e6583
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-26 14:09:32 -0500

    un avance es un avance.

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,3 +1,6 @@
 //All pokemon state
 export * from './GeneralPokemonInfoType';
 export * from './AllPokemonGeneralInfoStateType';
+
+//custom pokemon type
+export * from './CustomSinglePokemonType';
