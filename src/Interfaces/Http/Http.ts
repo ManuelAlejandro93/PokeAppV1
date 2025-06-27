4cf0c2d3c9b098d7eda63dbcfcd850927ff16b94
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-27 10:51:41 -0500

    organizando la forma del estado.

--- src/Interfaces/Http/Http.ts
+++ src/Interfaces/Http/Http.ts
@@ -1,5 +1,10 @@
 export interface Http {
-  status: 'fulfilled' | 'rejected' | 'pending' | 'checking-on-storage';
+  status:
+    | 'fulfilled'
+    | 'rejected'
+    | 'pending'
+    | 'checking-on-storage'
+    | 'non-requested';
   hasError: boolean;
   errorMessage: boolean;
 }
