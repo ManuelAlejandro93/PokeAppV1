883af5031f3f0670ce1b9e05c2c85dfe27359037
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-06-29 16:46:30 -0500

    Modal- paso7 -listo.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@/Store';
 import { useGetAllPokemonInfoHook } from '@/Hooks';
-import { SkeletonComponent, PokemonTable } from '@/Components';
+import { SkeletonComponent, PokemonTable, Modal } from '@/Components';
 import { CustomSinglePokemon } from '@/Interfaces';
 
 export const HomePage = () => {
@@ -14,12 +14,13 @@ export const HomePage = () => {
     <div className='min-h-screen bg-gray-50 p-8'>
       <h1 className='text-3xl font-bold text-center mb-8'>
         Pokédex - Primera Generación
-        {!data || data === null ? (
-          <SkeletonComponent />
-        ) : (
-          <PokemonTable data={data as CustomSinglePokemon[]} />
-        )}
       </h1>
+      {!data || data === null ? (
+        <SkeletonComponent />
+      ) : (
+        <PokemonTable data={data as CustomSinglePokemon[]} />
+      )}
+      <Modal />
     </div>
   );
 };
